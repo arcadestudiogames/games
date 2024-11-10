ea01989898bb9c69e04623bb7b9f73cbb203d698
CaVaAnWl <arcade.studio.jeux@gmail.com>
2024-11-10 15:02:23 +0100

    Update mail.js

--- mail.js
+++ mail.js
@@ -9,20 +9,7 @@ function sendEmail() {
   } 
   else {
     var body = "nom: " + nom + "\n" + "prenom: " + prenom + "\n" + "email: " + mail + "\n" + "message: " + messagecontact;
-    var objet = "contact de la part de " + prenom + " " + nom;
+    var objet = "Contact de la part de " + mail;
   }
-
-    Email.send({
-      Host: "smtp.gmail.com",
-      Username: "arcade.studio.jeux@gmail.com",
-      Password: "Arcade2011!",
-      To: 'arcade.studio.jeux@gmail.com',
-      From: "arcade.studio.jeux@gmail.com",
-      Subject: objet,
-      Body: body+" "+mail+" "+nom+" "+prenom
-    }).then(
-      message => alert("Votre message contenant " + message + " nous a été envoyé avec succès !")
-    ).catch(
-      error => alert("Erreur lors de l'envoi de votre message : " + error)
-    );
+  window.location.href="mailto:"+arcade.studio.jeux@gmail.com+"?cc="+mail+"?subject="+objet+"&body="+body;
 }
